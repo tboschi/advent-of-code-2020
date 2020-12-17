80a7e73e441cccb105f107f6510f27855836cc5f
Tommaso Boschi <boschi.tommaso@gmail.com>
2020-12-17 09:04:26 -0800

    day 15 only in C++, 10s execution time

---
+++ day15/README.md
@@ -0,0 +1,14 @@
+# Day 15
+
+The puzzle for [Day 15](https://adventofcode.com/2020/day/15)
+
+## Compile and get the solution
+
+For the C++ implementation
+```
+make cpp
+./cpp/part1 input
+./cpp/part2 input
+```
+
+Python implementation coming soon..
---
+++ day15/cpp/src/part2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+int main(int argc, char** argv) {
+	if (argc < 1) {
+		std::cerr << "Input file expected\n";
+		return 1;
+	}
+
+	// read file
+	std::ifstream infile(argv[1]);
+	if (!infile.good()) {
+		std::cerr << "Input file " << argv[1] << " does not exist\n";
+		return 1;
+	}
+
+	size_t c = 0;
+	int speak = 0, last = -1;
+
+	std::string line;
+	std::unordered_map<int, int> rounds;
+	while (std::getline(infile, line, ',')) {
+		if (c > 0)
+			rounds[last] = c;
+		++c;
+
+		speak = std::stoi(line);
+		std::cout << c << " say: " << speak << "\n";
+		last = speak;
+	}
+
+	for (size_t i = c; i < 30000000; ++i) {
+		auto rl = rounds.find(last);
+		if (rl == rounds.end()) {
+			rounds.insert( {last, i});
+			last = 0;
+			//std::cout << i+1 << " say: " << 0 << "\n";
+		}
+		else {
+			last = i - rl->second;
+			rl->second = i;
+			//std::cout << i+1 << " say: " << last << "\n";
+		}
+	}	
+
+	std::cout << "Last round is " << last << "\n";
+
+	return 0;
+}
